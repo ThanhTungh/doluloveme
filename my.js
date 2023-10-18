a886669cced9814eb07862442848ae9ac49cbe95
ThanhTungh <tungtottinh555@gmail.com>
2023-10-18 19:38:26 +0700

    first commit

---
+++ my.js
@@ -0,0 +1,8 @@
+function hoverNoButton(){
+    let btnNo = document.getElementById('no-btn');
+    btnNo.style.top = Math.random()*500 +"px";
+    btnNo.style.left = Math.random()*500 +"px";
+}
+function clickYesButton(){
+    alert('Ok')
+}
